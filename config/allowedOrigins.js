2236807f65800b2c5dbce3562601d569e7171fca
JUAN JOSE LANGA MATA <juanjoselangamata@JUANs-MBP.home>
2023-07-11 12:45:39 +0200

    Allowed origins fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,4 @@
-const allowedOrigins = ['https://technotes.onrender.com'];
+// const allowedOrigins = ['https://technotes.onrender.com'];
+const allowedOrigins = ['https://technotes-fe-4mw9.onrender.com/'];
 
 module.exports = allowedOrigins;
